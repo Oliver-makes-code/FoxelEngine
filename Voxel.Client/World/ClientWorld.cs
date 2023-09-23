ac76d7d451c0bb9b3896f3a71fb06828bb7241d5
Octal <oliver@olivermakesco.de>
2023-09-23 12:31:10 -0500

    waaa

--- Voxel.Client/World/ClientWorld.cs
+++ Voxel.Client/World/ClientWorld.cs
@@ -95,7 +95,7 @@ public class ClientWorld {
     public void Draw(Effect effect, Camera camera) {
         if (!Monitor.TryEnter(loadedChunks, 0))
             return;
-        var chunks = loadedChunks.OrderBy(it => camera.DistanceTo(it.Key.ToVector()));
+        var chunks = loadedChunks.OrderBy(it => camera.DistanceTo(it.Key.ToVector())).ToArray();
         Monitor.Exit(loadedChunks);
 
         foreach (var pair in chunks) {
