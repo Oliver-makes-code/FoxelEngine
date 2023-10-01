2cd85b231f525a4368f6b50822611d70ff383408
Octal <oliver@olivermakesco.de>
2023-10-01 13:06:59 -0500

    Get engine running again with monogame, to use as a reference

--- Voxel/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
-using RenderSurface;
-using Voxel.Client;
-
-using var game = new VoxelNewClient();
-
-game.Run(20, "Voxel Game");
