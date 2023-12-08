9164e912982c4099a9773dda2328edf846e71f61
Cassunshine <cass.stark.sunshine@gmail.com>
2023-12-08 10:26:35 -0500

    -Remove VTagged for now

--- Common/Util/Serialization/Tagged/VTaggedSerializationManager.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.Util.Serialization.Tagged;
-
-public static class VTaggedSerializationManager {
-    private static readonly Dictionary<Type, VTaggedTypeSpec> TypeSpecs = new Dictionary<Type, VTaggedTypeSpec>();
-    
-    
-    
-}
