e20e885ea8dce2e831835fb5ad71dff2b3e72d59
Emily <louis_d@live.com>
2024-01-08 18:44:44 -0500

    Make the setting code more generic

---
+++ Common/World/WorldSetting.cs
@@ -0,0 +1,8 @@
+﻿using Voxel.Common.Util;
+
+namespace Voxel.Common.World;
+
+public class WorldSetting<TValue> : Setting<TValue> {
+    public WorldSetting(string group, string value) : base("World", group, value) { }
+    public WorldSetting(string group, string value, TValue defaultValue) : base("World", group, value, defaultValue) { }
+}
