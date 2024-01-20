20aa0a74ffa84ab7eec37082fb52f24dfac65818
Oliver-makes-code <oliver@olivermakesco.de>
2024-01-20 16:13:15 -0600

    Implement Cass's suggestions (Help I was held at gunpoint)

--- Common/Collections/RollingQueue.cs
+++ Common/Collections/RollingQueue.cs
@@ -2,45 +2,36 @@ using System.Collections;
 
 namespace Voxel.Common.Collections;
 
-public class RollingQueue<T> : IEnumerable<HashSet<T>>, IEnumerable {
-    public HashSet<T> this[int index] {
-        get => CreateIfEmpty(index);
-        set => AppendEach(index, value);
-    }
+public class RollingQueue<T> : IEnumerable<List<T>>, IEnumerable {
+    private readonly Queue<List<T>> InternalHolder = [];
 
-    private Dictionary<int, HashSet<T>> internalHolder = [];
-
-    public HashSet<T> Shift() {
-        var output = this[0];
-
-        var next = new Dictionary<int, HashSet<T>>();
-
-        foreach (var elem in internalHolder) {
-            var newKey = elem.Key-1;
-            if (newKey >= 0)
-                next[newKey] = elem.Value;
-        }
-
-        internalHolder = next;
-
-        return output;
+    public void Shift(List<T> input) {
+        if (InternalHolder.Count == 0)
+            return;
+        var output = InternalHolder.Dequeue();
+        input.AddRange(output);
+        output.Clear();
+        InternalHolder.Enqueue(output);
     }
 
     public void Append(int index, T value)
-        => this[index].Add(value);
+        => CreateIfEmpty(index).Add(value);
     
-    public void AppendEach(int index, HashSet<T> value)
-        => this[index].UnionWith(value);
+    public void AppendEach(int index, IEnumerable<T> values) {
+        var list = CreateIfEmpty(index);
+        list.AddRange(values);
+    }
 
-    public IEnumerator<HashSet<T>> GetEnumerator()
-        => throw new NotImplementedException();
+    public IEnumerator<List<T>> GetEnumerator()
+        => InternalHolder.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator()
         => GetEnumerator();
 
-    private HashSet<T> CreateIfEmpty(int index) {
-        if (!internalHolder.ContainsKey(index))
-            internalHolder[index] = [];
-        return internalHolder[index];
+    private List<T> CreateIfEmpty(int index) {
+        while (InternalHolder.Count <= index)
+            InternalHolder.Enqueue([]);
+        
+        return InternalHolder.ElementAt(index);
     }
 }
