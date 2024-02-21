f0595f91195dfaae22766a71769407f06d86ca37
Oliver-makes-code <oliver@olivermakesco.de>
2024-02-21 04:16:38 -0600

    Get NoBraceNewline checker working

--- Codestyle/Checkers/NoBraceNewline.cs
+++ Codestyle/Checkers/NoBraceNewline.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Voxel.Codestyle.Checkers;
@@ -18,12 +19,30 @@ public class NoBraceNewline : SyntaxTreeChecker {
 
     public override void Check(SyntaxTreeAnalysisContext context) {
         foreach (var token in Find(context, SyntaxKind.OpenBraceToken)) {
-            if (token.LeadingTrivia.Any(it => it.Token.ValueText.Contains('\n'))) {
+            var prev = token.GetPreviousToken();
+            SyntaxKind[] valid = [
+                SyntaxKind.CloseParenToken,
+                SyntaxKind.CloseBracketToken,
+                SyntaxKind.EqualsGreaterThanToken,
+                SyntaxKind.EqualsToken,
+                SyntaxKind.GetKeyword,
+                SyntaxKind.SetKeyword,
+                SyntaxKind.UnsafeKeyword
+            ];
+            var isValid = false;
+            
+            foreach (var v in valid)
+                if (prev.IsKind(v))
+                    isValid = true;
+            
+            if (!isValid)
+                return;
+
+            if (token.LeadingTrivia.Any(it => it.IsKind(SyntaxKind.EndOfLineTrivia))) {
                 Diagnose(context, token.GetLocation());
                 return;
             }
-            var prev = token.GetPreviousToken();
-            if (prev.TrailingTrivia.Any(it => it.Token.ValueText.Contains('\n'))) {
+            if (prev.TrailingTrivia.Any(it => it.IsKind(SyntaxKind.EndOfLineTrivia))) {
                 Diagnose(context, token.GetLocation());
                 return;
             }
