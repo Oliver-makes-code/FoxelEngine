1c7ebf30d22556a28485d57a01d55e5e75bde47e
Oliver-makes-code <oliver@olivermakesco.de>
2024-03-18 00:36:48 -0500

    Clean up unused functions in terrain shader

--- Client/Content/shaders/terrain.v.glsl
+++ Client/Content/shaders/terrain.v.glsl
@@ -24,31 +24,16 @@ void Unpack(int packedColor, int packedUv, int packedUvMin, int packedUvMax, out
     UnpackUv(packedUvMax, uvMax);
 }
 
-float singleInterp(float strength, float baseColor) {
-    float min = 0.95 * baseColor * baseColor;
-    return min + (baseColor - min) * strength;
-}
-
-vec3 getColorMultiplier(float strength, vec3 baseColor) {
-    return vec3(
-        singleInterp(strength, baseColor.x),
-        singleInterp(strength, baseColor.y),
-        singleInterp(strength, baseColor.z)
-    );
-}
-
-void vert(vec3 position, int packedColor, int packedUv, int packedUvMin, int packedUvMax, out vec3 o_color, out vec2 o_uv, out float o_distance, out vec2 o_uvMin, out vec2 o_uvMax){
+void vert(vec3 position, int packedColor, int packedUv, int packedUvMin, int packedUvMax, out vec3 o_color, out vec2 o_uv, out vec2 o_uvMin, out vec2 o_uvMax){
     vec4 color;
     Unpack(packedColor, packedUv, packedUvMin, packedUvMax, color, o_uv, o_uvMin, o_uvMax);
     o_color = colorBlendUniform(color.rgb, vec3(0), color.a);
 
     vec4 pos = ModelVertex(position);
     gl_Position = pos;
-    o_distance = pos.z;
 }
 
-void frag(vec3 color, vec2 uv, float distance, vec2 uvMin, vec2 uvMax, out vec4 o_color){
+void frag(vec3 color, vec2 uv, vec2 uvMin, vec2 uvMax, out vec4 o_color){
     vec4 sampledColor = colorBlendAverage(interpolatePixels(uv, uvMin, uvMax, Texture, TextureSampler));
     o_color = vec4(colorBlendUniform(sampledColor.rgb, sampledColor.rgb * color, 0.15), sampledColor.a);
-    //o_gbuffer = vec4(1, 1, 0, 1);
 }
