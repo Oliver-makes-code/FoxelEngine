d38a9a39442e63b1fe6a6f2d16155096a9a9a4d5
Oliver-makes-code <oliver@olivermakesco.de>
2024-09-06 03:21:05 -0500

    Default state + make block properties better

--- Common/World/Content/Blocks/Block.cs
+++ Common/World/Content/Blocks/Block.cs
@@ -4,12 +4,17 @@ namespace Foxel.Common.World.Content.Blocks;
 
 public class NewBlock {
     public readonly BlockStateMap Map;
+    public readonly BlockState DefaultState;
 
     public NewBlock() {
         var builder = new BlockStateMap.Builder();
         AddStates(builder);
         Map = builder.Build();
+        DefaultState = DefineDefaultState();
     }
+
+    public virtual BlockState DefineDefaultState()
+        => new(this);
 
     public virtual void AddStates(BlockStateMap.Builder builder) {}
 }
--- Common/World/Content/Blocks/State/BlockProperty.cs
+++ Common/World/Content/Blocks/State/BlockProperty.cs
@@ -2,15 +2,37 @@ namespace Foxel.Common.World.Content.Blocks.State;
 
 public interface BlockProperty {
     public byte GetPropertyCount();
+
     public string GetName();
-}
 
-public interface BlockProperty<TValue> : BlockProperty where TValue : struct  {
-    public TValue GetValue(byte index);
-
-    public byte GetIndex(TValue value);
+    public object GetValueObject(byte index);
+    
+    public byte GetIndexObject(object value);
 
     public bool ValidIndex(byte index);
 
-    public bool ValidValue(TValue value);
+    public bool ValidValueObject(object value);
+}
+
+public abstract record BlockProperty<TValue> : BlockProperty where TValue : struct  {
+    public object GetValueObject(byte index)
+        => GetValue(index);
+
+    public byte GetIndexObject(object value)
+        => GetIndex((TValue)value);
+
+    public bool ValidValueObject(object value)
+        => ValidValue((TValue)value);
+
+    public abstract TValue GetValue(byte index);
+
+    public abstract byte GetIndex(TValue value);
+
+    public abstract bool ValidValue(TValue value);
+
+    public abstract byte GetPropertyCount();
+
+    public abstract string GetName();
+
+    public abstract bool ValidIndex(byte index);
 }
