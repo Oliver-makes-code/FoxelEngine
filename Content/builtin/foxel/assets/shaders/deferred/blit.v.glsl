9073ff0e5822040b148efe4731c0d4cf052d4f89
Oliver-makes-code <oliver@olivermakesco.de>
2024-09-09 01:32:20 -0500

    Quad screen thing for viewing the different render textures

--- Content/builtin/foxel/assets/shaders/deferred/blit.v.glsl
+++ Content/builtin/foxel/assets/shaders/deferred/blit.v.glsl
@@ -26,7 +26,18 @@ void vert() {
 #ifdef FRAGMENT
 
 void frag() {
-    o_Color = texture(sampler2D(ColorTexture, ColorTextureSampler), fs_Uv);
+    vec2 uv = fs_Uv * 2;
+    if (uv.x < 1 && uv.y < 1) {
+        o_Color = texture(sampler2D(ColorTexture, ColorTextureSampler), uv);
+    } else if (uv.x < 1) {
+        vec4 color = texture(sampler2D(NormalTexture, NormalTextureSampler), uv - vec2(0, 1));
+        o_Color = vec4((color.rgb + 1) * 0.5, color.a);
+    } else if (uv.y < 1) {
+        o_Color = texture(sampler2D(PositionTexture, PositionTextureSampler), uv - vec2(1, 0));
+    } else {
+        float depth = texture(sampler2D(DepthTexture, DepthTextureSampler), uv - vec2(1, 1)).r;
+        o_Color = vec4(depth, depth, depth, 1);
+    }
 }
 
 #endif
